ba7a3057a144e81ed9b2cccda4eba0709f66f783
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-05-03 16:03:28 -0300

    feat: create sign up page and form input component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { toRefs, type InputTypeHTMLAttribute } from 'vue';
+
+const model = defineModel<string>();
+
+const props = defineProps<{
+  type: InputTypeHTMLAttribute
+  placeholder: string
+  id: string
+}>();
+
+const { placeholder, type } = toRefs(props);
+
+</script>
+
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    :id="id"
+    v-model="model"
+    required="true"
+    class="
+      border-2 focus:border-indigo-800 hover:border-indigo-500
+      p-2 rounded-sm outline-none
+    "
+  />
+</template>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,6 +1,64 @@
 <script setup lang="ts">
+import type { User } from '@/types/user';
+import FormInput from '../components/FormInput.vue';
+import { reactive } from 'vue';
+
+const user = reactive<User>({
+  email: "",
+  password: "",
+  name: "",
+});
+
+const handleForm = () => console.table({
+  name: user.name,
+  email: user.email,
+  password: user.password,
+});
 </script>
 
 <template>
-  <h1>Hello, Inuc!</h1>
+  <main class="flex justify-center items-center h-screen">
+    <form
+      class="
+        flex flex-col
+        bg-indigo-300 p-6 rounded space-y-6
+        w-11/12 max-w-xs
+      "
+      v-on:submit.prevent="handleForm"
+    >
+      <header>
+        <h1 class="text-xl font-medium text-zinc-900">Crie sua conta</h1>
+      </header>
+
+      <div class="flex flex-col gap-2">
+        <FormInput
+          id="username"
+          type="text"
+          placeholder="Nome de usuário"
+          v-model="user.name"
+        />
+        <FormInput
+          id="email"
+          type="email"
+          placeholder="E-mail"
+          v-model="user.email"
+        />
+        <FormInput
+          id="password"
+          type="password"
+          placeholder="Senha"
+          v-model="user.password"
+        />
+      </div>
+
+      <button
+        class="
+          bg-indigo-800 hover:bg-indigo-700 active:bg-indigo-600
+          p-2 rounded-sm text-white
+        "
+      >
+        Criar conta
+      </button>
+    </form>
+  </main>
 </template>
