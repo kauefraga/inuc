797ec13aab9e3da9504cb05637d43a6cdef59ac1
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-07-06 15:35:24 -0300

    feat: migrate to react with vite

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>BOMDIA</h1>
+    </>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,vue}"],
+  content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {},
   },
