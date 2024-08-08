82072bc13ca2a21fbe9bb316c09cf2915f2d0eba
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-08 08:38:06 -0300

    feat: set QueryClient and use the QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HomePage, CreateUserPage } from './pages/index';
 import './main.css';
 
@@ -16,8 +17,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
