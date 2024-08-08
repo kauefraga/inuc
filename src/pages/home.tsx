2d89e457c2a007d2d237c80ad4160d0e8a343f7e
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-08 07:48:56 -0300

    feat: increase Home wrapper and its children padding

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <main className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-8 rounded-lg bg-zinc-900 p-8 text-2xl text-white md:flex-row">
+      <div className="flex flex-col items-center gap-8 rounded-lg bg-zinc-900 p-10 text-2xl text-white md:flex-row">
         <Link
           to="/user/login"
-          className="p-3 decoration-2 underline-offset-2 hover:underline hover:decoration-yellow-400"
+          className="p-4 decoration-2 underline-offset-2 hover:underline hover:decoration-yellow-400"
         >
           Entrar
         </Link>
         <Link
           to="/user/create"
-          className="rounded bg-violet-800 p-3 transition-colors hover:bg-violet-700"
+          className="rounded bg-violet-800 p-4 transition-colors hover:bg-violet-700"
         >
           Criar conta
         </Link>
